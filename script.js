f4659da8dd78080f2759cfb4ef4d4e8ad117e8fc
Otary <oto-101@mail.ru>
2024-09-05 23:09:06 +0300

    fgfg

--- script.js
+++ script.js
@@ -187,6 +187,13 @@ var Visible_text = function (target) {
             choice_block_text_three.classList.add('text_active');
         }
     }
+    if (choice_block_line_two.classList.contains('line_active') && choice_block_text_two.classList.contains('text_active')) {
+        choice_block_line_three.classList.remove('line_active');
+        choice_block_text_three.classList.remove('text_active');
+    } else {
+        choice_block_line_three.classList.add('line_active');
+        choice_block_text_three.classList.add('text_active');
+    }
 
 };
 container_content.addEventListener('scroll', function () {
