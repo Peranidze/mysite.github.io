649913613663ed14d6f84db2c352625d6472a5b2
Otary <oto-101@mail.ru>
2024-09-09 22:02:46 +0300

    dfgfdg

--- script.js
+++ script.js
@@ -199,8 +199,22 @@ var Visible_text = function (target) {
             choice_block_line_three.classList.add('line_active');
             choice_block_text_three.classList.add('text_active');
         }
-    }
 
+        if (choice_block_line_two.classList.contains('line_active') && choice_block_text_two.classList.contains('text_active')) {
+            choice_block_line_three.classList.remove('line_active');
+            choice_block_text_three.classList.remove('text_active');
+        } else {
+            choice_block_line_three.classList.add('line_active');
+            choice_block_text_three.classList.add('text_active');
+        }
+    }
+    if (choice_block_line_one.classList.contains('line_active') && choice_block_text_one.classList.contains('text_active')) {
+        choice_block_line_two.classList.remove('line_active');
+        choice_block_text_two.classList.remove('text_active');
+    } else {
+        choice_block_line_two.classList.add('line_active');
+        choice_block_text_two.classList.add('text_active');
+    }
     // console.log(targetPosition.top, check_point_h)
     // if (Math.round(targetPosition.top) >= Math.round(check_point_h)) {
     //     choice_block_line_one.classList.add('line_active');
